d25b342109959f5ed4099fa25e94d169087ebfd0
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-08-06 17:43:47 +0530

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Test Ios on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
